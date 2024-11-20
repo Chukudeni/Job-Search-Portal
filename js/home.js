6ad68891d904e518beb817fa600b5beda105aca3
Chukudeni <117643715+Chukudeni@users.noreply.github.com>
2024-11-20 15:44:57 +0100

    final final version

--- js/home.js
+++ js/home.js
@@ -1,7 +1,7 @@
 function recentVac(){
     //taking input from user to search with api and collect data from the
     // api database
-    fetch(`http://api.lmiforall.org.uk/api/v1/vacancies/search?location=&keywords=`)
+    fetch("http://api.lmiforall.org.uk/api/v1/vacancies/search")
     .then(
         response => response.ok ? response.json() : Promise.reject("Failed to fetch information")
     )
@@ -9,22 +9,22 @@ function recentVac(){
         data =>{
             //looping through array collected from api to get data and 
             // print to HTML
-            for (let index = 20; index < 10; index++) {
+            for (let index = 0; index < 10; index++) {
                 const element = data[index];
                 
-                document.getElementById("vacancy").innerText += '\n'+'\n'
+                // document.getElementsByClassName("recent").innerHTML 
+                document.querySelector(".recent").innerText
+                += '\n'+'\n'
                 +'\n' + "Title: " + element.title
-                +'\n' + "Summary: " + element.summary
                 +'\n'+'\n' + "Company: " + element.company
-                +'\n'+'\n' + "Start time: " + element.activedate.start
-                +'\n'+'\n' + "End time: " + element.activedate.end
                 +'\n'+'\n' + "Location: " + element.location.location
                 +'\n'+'\n' + "Job Link: " + element.link
+                
             }
 
 
         }
     ).catch(() => {
-          document.querySelector("#searchBox").innerHTML = "Sorry, could not get information try again"
+          document.querySelector(".recent").innerHTML = "Sorry, could not get information try again"
     })
 }
